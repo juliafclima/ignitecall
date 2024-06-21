79d42bb0906e5032b5f4a38c4919f57e314881df
Júlia Lima <juliafclima@hotmail.com>
2024-06-21 11:52:19 -0300

    Conteúdo da sessão do Next Auth

--- src/pages/api/auth/[...nextAuth].api.ts
+++ src/pages/api/auth/[...nextAuth].api.ts
@@ -44,6 +44,12 @@ export function buildNextAuthOptions(
 
         return true;
       },
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        };
+      },
     },
   };
 }
