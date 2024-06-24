44493efac6a7aa79397683ab95ea3e45698afa1e
Júlia Lima <juliafclima@hotmail.com>
2024-06-24 13:22:40 -0300

    Criação do evento no Google

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -1,34 +1,36 @@
-import { NextApiRequest, NextApiResponse, NextPageContext } from 'next'
-import { Adapter } from 'next-auth/adapters'
-import { parseCookies, destroyCookie } from 'nookies'
-import { prisma } from '../prisma'
+import { NextApiRequest, NextApiResponse, NextPageContext } from "next";
+import { destroyCookie, parseCookies } from "nookies";
+
+import { Adapter } from "next-auth/adapters";
+import { prisma } from "../prisma";
 
 export function PrismaAdapter(
-  req: NextApiRequest | NextPageContext['req'],
-  res: NextApiResponse | NextPageContext['res'],
+  req: NextApiRequest | NextPageContext["req"],
+  res: NextApiResponse | NextPageContext["res"]
 ): Adapter {
   return {
     async createUser(user) {
-      const { '@ignitecall:userId': userIdOnCookies } = parseCookies({ req })
+      const { "@ignitecall:userId": userIdOnCookies } = parseCookies({ req });
 
       if (!userIdOnCookies) {
-        throw new Error('User ID not found on cookies.')
+        throw new Error("User ID not found on cookies.");
       }
 
       const prismaUser = await prisma.user.update({
         where: {
           id: userIdOnCookies,
         },
+        
         data: {
           name: user.name,
           email: user.email,
           avatar_url: user.avatar_url,
         },
-      })
+      });
 
-      destroyCookie({ res }, '@ignitecall:userId', {
-        path: '/',
-      })
+      destroyCookie({ res }, "@ignitecall:userId", {
+        path: "/",
+      });
 
       return {
         id: prismaUser.id,
@@ -37,7 +39,7 @@ export function PrismaAdapter(
         email: prismaUser.email!,
         emailVerified: null,
         avatar_url: prismaUser.avatar_url!,
-      }
+      };
     },
 
     async getUser(id) {
@@ -45,10 +47,10 @@ export function PrismaAdapter(
         where: {
           id,
         },
-      })
+      });
 
       if (!user) {
-        return null
+        return null;
       }
 
       return {
@@ -58,17 +60,17 @@ export function PrismaAdapter(
         email: user.email!,
         emailVerified: null,
         avatar_url: user.avatar_url!,
-      }
+      };
     },
     async getUserByEmail(email) {
       const user = await prisma.user.findUnique({
         where: {
           email,
         },
-      })
+      });
 
       if (!user) {
-        return null
+        return null;
       }
 
       return {
@@ -78,7 +80,7 @@ export function PrismaAdapter(
         email: user.email!,
         emailVerified: null,
         avatar_url: user.avatar_url!,
-      }
+      };
     },
     async getUserByAccount({ providerAccountId, provider }) {
       const account = await prisma.account.findUnique({
@@ -88,16 +90,17 @@ export function PrismaAdapter(
             provider_account_id: providerAccountId,
           },
         },
+        
         include: {
           user: true,
         },
-      })
+      });
 
       if (!account) {
-        return null
+        return null;
       }
 
-      const { user } = account
+      const { user } = account;
 
       return {
         id: user.id,
@@ -106,7 +109,7 @@ export function PrismaAdapter(
         email: user.email!,
         emailVerified: null,
         avatar_url: user.avatar_url!,
-      }
+      };
     },
 
     async updateUser(user) {
@@ -114,12 +117,13 @@ export function PrismaAdapter(
         where: {
           id: user.id!,
         },
+        
         data: {
           name: user.name,
           email: user.email,
           avatar_url: user.avatar_url,
         },
-      })
+      });
 
       return {
         id: prismaUser.id,
@@ -128,7 +132,7 @@ export function PrismaAdapter(
         email: prismaUser.email!,
         emailVerified: null,
         avatar_url: prismaUser.avatar_url!,
-      }
+      };
     },
 
     async linkAccount(account) {
@@ -146,7 +150,7 @@ export function PrismaAdapter(
           id_token: account.id_token,
           session_state: account.session_state,
         },
-      })
+      });
     },
 
     async createSession({ sessionToken, userId, expires }) {
@@ -156,13 +160,13 @@ export function PrismaAdapter(
           expires,
           session_token: sessionToken,
         },
-      })
+      });
 
       return {
         userId,
         sessionToken,
         expires,
-      }
+      };
     },
 
     async getSessionAndUser(sessionToken) {
@@ -170,16 +174,17 @@ export function PrismaAdapter(
         where: {
           session_token: sessionToken,
         },
+        
         include: {
           user: true,
         },
-      })
+      });
 
       if (!prismaSession) {
-        return null
+        return null;
       }
 
-      const { user, ...session } = prismaSession
+      const { user, ...session } = prismaSession;
 
       return {
         session: {
@@ -187,6 +192,7 @@ export function PrismaAdapter(
           expires: session.expires,
           sessionToken: session.session_token,
         },
+        
         user: {
           id: user.id,
           name: user.name,
@@ -195,7 +201,7 @@ export function PrismaAdapter(
           emailVerified: null,
           avatar_url: user.avatar_url!,
         },
-      }
+      };
     },
 
     async updateSession({ sessionToken, userId, expires }) {
@@ -203,17 +209,18 @@ export function PrismaAdapter(
         where: {
           session_token: sessionToken,
         },
+        
         data: {
           expires,
           user_id: userId,
         },
-      })
+      });
 
       return {
         sessionToken: prismaSession.session_token,
         userId: prismaSession.user_id,
         expires: prismaSession.expires,
-      }
+      };
     },
 
     async deleteSession(sessionToken) {
@@ -221,7 +228,7 @@ export function PrismaAdapter(
         where: {
           session_token: sessionToken,
         },
-      })
+      });
     },
-  }
+  };
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,15 +1,32 @@
 import { globalCss } from '@ignite-ui/react'
 
 export const globalStyles = globalCss({
-  '*': {
-    boxSizing: 'border-box',
+  "*": {
+    boxSizing: "border-box",
     padding: 0,
     margin: 0,
   },
 
   body: {
-    backgroundColor: '$gray900',
-    color: '$gray100',
-    '-webkit-font-smoothing': 'antialiased',
+    backgroundColor: "$gray900",
+    color: "$gray100",
+    "-webkit-font-smoothing": "antialiased",
+
+    "&::-webkit-scrollbar": {
+      width: "8px", // width of the scrollbar
+    },
+
+    "&::-webkit-scrollbar-track": {
+      background: "$gray700", 
+      borderRadius: "10px",
+    },
+
+    "&::-webkit-scrollbar-thumb": {
+      background: "$gray500", 
+      borderRadius: "10px",
+      "&:hover": {
+        background: "$gray400",
+      },
+    },
   },
-})
+});
